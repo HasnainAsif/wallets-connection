b2284c0b4487ce6e24f201956de29ba1c804532e
Hasnain Asif <hasnainasif@MP-HasnainA-SE-363.local>
2021-11-15 18:08:46 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <button>Connect</button>
+      <button>Disconnect</button>
+    </div>
+  );
+}
+
+export default App;
